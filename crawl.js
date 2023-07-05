c90ff1a4eaf765ac391e479965091600cf1ca670
Codie@123 <elliott6518@gmail.com>
2023-07-05 16:00:22 +0530

    Third Commite
    getHtmlFromBody function , used JSDOM  ,tested with different edge-cases

--- crawl.js
+++ crawl.js
@@ -1,3 +1,35 @@
+const { JSDOM } = require("jsdom");
+
+const getHtmlFromUrl = (htmlBody, baseUrl) => {
+  const urls = [];
+  const dom = new JSDOM(htmlBody);
+  const linkElements = dom.window.document.querySelectorAll("a");
+
+  for (const linkElement of linkElements) {
+    if (linkElement.href.slice(0, 1) === "/") {
+      // relative
+      try {
+        const urlObj = new URL(`${baseUrl}${linkElement.href}`);
+
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.log(`Error with Relative  Url ${err.message}`);
+      }
+    } else {
+      // absolute
+      try {
+        const urlObj = new URL(`${linkElement.href}`);
+
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.log(`Error with Absolute  Url ${err.message}`);
+      }
+    }
+    // console.log(linkElement.href);
+  }
+  return urls;
+};
+
 const normalizeURL = (urlstring) => {
   const urlObj = new URL(urlstring);
 
@@ -11,4 +43,5 @@ const normalizeURL = (urlstring) => {
 
 module.exports = {
   normalizeURL,
+  getHtmlFromUrl,
 };
--- crawl.test.js
+++ crawl.test.js
@@ -1,5 +1,5 @@
 const { test, expect } = require("@jest/globals");
-const { normalizeURL } = require("./crawl.js");
+const { normalizeURL, getHtmlFromUrl } = require("./crawl.js");
 
 test("NormalizeUrl Strip protocol ", () => {
   const input = "https://thecerc.com/work.html";
@@ -24,4 +24,80 @@ test("NormalizeUrl Strip http", () => {
   const actualUrl = normalizeURL(input);
   const expected = "thecerc.com/work.html";
   expect(actualUrl).toEqual(expected);
+});
+
+test("getHtmlFromUrl ", () => {
+  const inputHtmlBody = `
+        <html>
+        <body>
+            <a href="https://thecerc.com/"><span>Go to TheCerc.tech</span></a>
+        </body>
+        </html>
+    `;
+  const baseUrl = "https://thecerc.com/";
+  const actual = getHtmlFromUrl(inputHtmlBody, baseUrl);
+  const expected = ["https://thecerc.com/"];
+  expect(actual).toEqual(expected);
+});
+
+test("getHtmlFromUrl Absolute Url", () => {
+  const inputHtmlBody = `
+          <html>
+          <body>
+              <a href="https://thecerc.com/work.html"><span>Go to TheCerc.tech</span></a>
+          </body>
+          </html>
+      `;
+  const baseUrl = "https://thecerc.com/work.html";
+  const actual = getHtmlFromUrl(inputHtmlBody, baseUrl);
+  const expected = ["https://thecerc.com/work.html"];
+  expect(actual).toEqual(expected);
+});
+
+test("getHtmlFromUrl Relative Url", () => {
+  const inputHtmlBody = `
+          <html>
+          <body>
+              <a href="/work.html"><span>Go to TheCerc.tech</span></a>
+          </body>
+          </html>
+      `;
+  const baseUrl = "https://thecerc.com";
+  const actual = getHtmlFromUrl(inputHtmlBody, baseUrl);
+  const expected = ["https://thecerc.com/work.html"];
+  expect(actual).toEqual(expected);
+});
+test("getHtmlFromUrl Both Url", () => {
+  const inputHtmlBody = `
+            <html>
+            <body>
+                <a href="/work1.html"><span>Go to TheCerc.tech</span></a>
+                <a href="https://thecerc.com/work2.html"><span>Go to TheCerc.tech</span></a>
+
+            </body>
+            </html>
+        `;
+  const baseUrl = "https://thecerc.com";
+  const actual = getHtmlFromUrl(inputHtmlBody, baseUrl);
+  const expected = [
+    "https://thecerc.com/work1.html",
+    "https://thecerc.com/work2.html",
+  ];
+  expect(actual).toEqual(expected);
+});
+
+test("getHtmlFromUrl Invalid", () => {
+  const inputHtmlBody = `
+            <html>
+            <body>
+                <a href="invalid "><span>Go to TheCerc.tech</span></a>
+             
+
+            </body>
+            </html>
+        `;
+  const baseUrl = "https://thecerc.com";
+  const actual = getHtmlFromUrl(inputHtmlBody, baseUrl);
+  const expected = [];
+  expect(actual).toEqual(expected);
 });
